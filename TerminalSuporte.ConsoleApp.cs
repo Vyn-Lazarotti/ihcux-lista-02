2fbe14705e7f646c23836631e7c0ef87abdba4c7
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-04-05 13:50:49 -0300

    TerminalSuporte.ConsoleApp.cs

--- TerminalSuporte.ConsoleApp.cs
+++ TerminalSuporte.ConsoleApp.cs
@@ -8,6 +8,7 @@ namespace TerminalSuporteConsoleApp
     {
       Console.WriteLine("Status do sistema: OPERACIONAL");
       Console.WriteLine("---------------------");
+      // Heurística #6: Reconhecimento em vez de Recordação
       Console.WriteLine("Comandos disponíveis: \n>PING - testar conexão \nRESET - reiniciar servidor (critico) \nHELP - ajuda \nEXIT - encerrar terminal");
       Console.WriteLine("Digite o comando: ");
       string comando = Console.ReadLine().ToUpper();
@@ -37,22 +38,22 @@ namespace TerminalSuporteConsoleApp
 
         case "RESET":
           Console.Clear();
-          Console.WriteLine("Você selecionou o reboot do servidor");
+          Console.WriteLine("Você selecionou o reboot do servidor");  // Heurística #10: Documentação contextual
           System.Threading.Thread.Sleep(2000); 
           Console.ForegroundColor = ConsoleColor.Yellow;
           Console.WriteLine("Deseja continuar? (1 para Sim/0 para Não): ");
           int resposta = Convert.ToInt32(Console.ReadLine());
           Console.ResetColor();
           Console.ReadKey();
-          if (resposta == 1)
+          if (resposta == 1) // Heurística #5: Prevenção de erros
           {
             Console.WriteLine("Reiniciando...");
             Console.WriteLine("Sistema online!");
             break;
-          } else if (resposta == 0)
+          } else if (resposta == 0) // Heurística #5: Prevenção de erros
           {
             break;
-          } else
+          } else // Heurística #5: Prevenção de erros
           {
             Console.WriteLine("Erro! Valor inválido!");
             break;
@@ -60,7 +61,7 @@ namespace TerminalSuporteConsoleApp
 
         case "HELP":
           Console.Clear();
-          Console.WriteLine("--- Central de Ajuda ---");
+          Console.WriteLine("--- Central de Ajuda ---");  // Heurística #10: Documentação contextual
           Console.WriteLine("- PING: Verifica se o servidor está funcionando");
           Console.WriteLine("- RESET: Reinicia o servidor");
           Console.WriteLine("- EXIT: Desliga o programa");
@@ -69,25 +70,25 @@ namespace TerminalSuporteConsoleApp
         case "EXIT":
           System.Threading.Thread.Sleep(2000); 
           Console.ForegroundColor = ConsoleColor.Red;
-          Console.WriteLine("Deseja continuar? (1 para Sim/0 para Não): ");
+          Console.WriteLine("Deseja continuar? (1 para Sim/0 para Não): ");  // Heurística #10: Documentação contextual
           int resposta2 = Convert.ToInt32(Console.ReadLine());
           Console.ResetColor();
           Console.ReadKey();
-          if (resposta2 == 1)
+          if (resposta2 == 1) // Heurística #5: Prevenção de erros
           {
             Console.WriteLine("Desligando...");
             break;
-          } else if (resposta2 == 0)
+          } else if (resposta2 == 0) // Heurística #5: Prevenção de erros
           {
             break;
-          } else
+          } else // Heurística #5: Prevenção de erros
           {
             Console.WriteLine("Erro! Valor inválido!");
             break;
           }
 
-        default:
-          Console.WriteLine("Comando inválido!");
+        default: // Heurística #5: Prevenção de erros
+          Console.WriteLine("Comando inválido!"); 
           break;
       } 
     }
